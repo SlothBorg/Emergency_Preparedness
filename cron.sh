79d9eed2320fae0c7ff4b5d2ecdd47c79517c43f
Slothborg <SlothBorg@users.noreply.github.com>
2023-01-06 08:19:30 -0500

    backup 06/01/2023

--- cron.sh
+++ cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget -mpck --html-extension --user-agent="" -e robots=off --wait 1 -P . http://www.kc2idf.com/Emergency_Preparedness/
-mv www.kc2idf.com/Emergency_Preparedness/* .
+cp -r www.kc2idf.com/Emergency_Preparedness/* .
 rm -rf www.kc2idf.com/
