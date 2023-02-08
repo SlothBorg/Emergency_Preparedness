2a5b5ef1d0cb4c8ec3e88aaafd02a786b393070f
Slothborg <SlothBorg@users.noreply.github.com>
2023-02-08 10:26:04 -0500

    2023-02-08

--- cron.sh
+++ cron.sh
@@ -3,3 +3,7 @@
 wget -mpck --html-extension --user-agent="" -e robots=off --wait 1 -P . http://www.kc2idf.com/Emergency_Preparedness/
 cp -r www.kc2idf.com/Emergency_Preparedness/* .
 rm -rf www.kc2idf.com/
+
+git add -A
+git commit -m $(date +%F)
+git push
