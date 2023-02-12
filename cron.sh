ae9e75c72349c5e4d9312065437a518bab615e18
Slothborg <SlothBorg@users.noreply.github.com>
2023-02-12 09:02:43 -0500

    2023-02-12

--- cron.sh
+++ cron.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ~/Code/Emergency_Preparedness
+
 wget -mpck --html-extension --user-agent="" -e robots=off --wait 1 -P . http://www.kc2idf.com/Emergency_Preparedness/
 cp -r www.kc2idf.com/Emergency_Preparedness/* .
 rm -rf www.kc2idf.com/
